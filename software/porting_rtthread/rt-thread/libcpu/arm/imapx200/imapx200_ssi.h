45785d9803745842ce658c88fed86d98e55861dd
xiaonong <nongxiaoming@gmail.com>
2014-11-24 01:48:50 +0800

    update the ssi register define.

--- software/porting_rtthread/rt-thread/libcpu/arm/imapx200/imapx200_ssi.h
+++ software/porting_rtthread/rt-thread/libcpu/arm/imapx200/imapx200_ssi.h
@@ -1,117 +1,41 @@
 #ifndef __IMAPX_SSI__
 #define __IMAPX_SSI__
 
-#define rSSI_CTLR0_M						 (0x0 )
-#define rSSI_CTLR1_M						 (0x4 )
-#define rSSI_ENR_M						 (0x8 )
-#define rSSI_MWCR_M						 (0xC )
-#define rSSI_SER_M						 (0x10 )
-#define rSSI_BAUDR_M						 (0x14 )
-#define rSSI_TXFTLR_M						 (0x18 )
-#define rSSI_RXFTLR_M						 (0x1C )
-#define rSSI_TXFLR_M						 (0x20 )
-#define rSSI_RXFLR_M						 (0x24 )
-#define rSSI_SR_M						 (0x28 )
-#define rSSI_IMR_M						 (0x2C )
-#define rSSI_ISR_M						 (0x30 )
-#define rSSI_RISR_M						 (0x34 )
-#define rSSI_TXOICR_M						 (0x38 )
-#define rSSI_RXOICR_M						 (0x3C )
-#define rSSI_RXUICR_M						 (0x40 )
-#define rSSI_MSTICR_M						 (0x44 )
-#define rSSI_ICR_M						 (0x48 )
-#define rSSI_DMACR_M						 (0x4C )
-#define rSSI_DMATDLR_M					     	 (0x50 )
-#define rSSI_DMARDLR_M					     	 (0x54 )
-#define rSSI_IDR_M						 (0x58 )
-#define rSSI_VERSION_M					     	 (0x5C )
-#define rSSI_DR_M						 (0x60 )
+#define MSSI0_BASE 0x20DF0000
+#define MSSI1_BASE 0x20DF1000
+#define MSSI2_BASE 0x20DF2000
+#define SSSI_BASE  0x20DF3000
 
+typedef struct
+{
+ unsigned long CTLR0;
+ unsigned long CTLR1;
+ unsigned long ENR;
+ unsigned long MWCR;
+ unsigned long SER;
+ unsigned long BAUDR;
+ unsigned long TXFTLR;
+ unsigned long RXFTLR;
+ unsigned long TXFLR;
+ unsigned long RXFLR;
+ unsigned long SR;
+ unsigned long IMR;
+ unsigned long ISR;
+ unsigned long RISR;
+ unsigned long TXOICR;
+ unsigned long RXOICR;
+ unsigned long RXUICR;
+ unsigned long MSTICR;
+ unsigned long ICR;
+ unsigned long DMACR;
+ unsigned long DMATDLR;
+ unsigned long DMARDLR;
+ unsigned long DR;
+}IMAP_SSI_TypeDef;
 
-
-#define rSSI_CTLR0_M_1						 (0x0 )
-#define rSSI_CTLR1_M_1						 (0x4 )
-#define rSSI_ENR_M_1						 (0x8 )
-#define rSSI_MWCR_M_1						 (0xC )
-#define rSSI_SER_M_1						 (0x10 )
-#define rSSI_BAUDR_M_1						 (0x14 )
-#define rSSI_TXFTLR_M_1						 (0x18 )
-#define rSSI_RXFTLR_M_1						 (0x1C )
-#define rSSI_TXFLR_M_1						 (0x20 )
-#define rSSI_RXFLR_M_1						 (0x24 )
-#define rSSI_SR_M_1						 (0x28 )
-#define rSSI_IMR_M_1						 (0x2C )
-#define rSSI_ISR_M_1						 (0x30 )
-#define rSSI_RISR_M_1						 (0x34 )
-#define rSSI_TXOICR_M_1						 (0x38 )
-#define rSSI_RXOICR_M_1						 (0x3C )
-#define rSSI_RXUICR_M_1						 (0x40 )
-#define rSSI_MSTICR_M_1						 (0x44 )
-#define rSSI_ICR_M_1						 (0x48 )
-#define rSSI_DMACR_M_1						 (0x4C )
-#define rSSI_DMATDLR_M_1					 (0x50 )
-#define rSSI_DMARDLR_M_1					 (0x54 )
-#define rSSI_IDR_M_1						 (0x58 )
-#define rSSI_VERSION_M_1					 (0x5C )
-#define rSSI_DR_M_1						 (0x60 )
-
-
-
-#define rSSI_CTLR0_M_2						 (0x0 )
-#define rSSI_CTLR1_M_2						 (0x4 )
-#define rSSI_ENR_M_2						 (0x8 )
-#define rSSI_MWCR_M_2						 (0xC )
-#define rSSI_SER_M_2						 (0x10 )
-#define rSSI_BAUDR_M_2						 (0x14 )
-#define rSSI_TXFTLR_M_2						 (0x18 )
-#define rSSI_RXFTLR_M_2						 (0x1C )
-#define rSSI_TXFLR_M_2						 (0x20 )
-#define rSSI_RXFLR_M_2						 (0x24 )
-#define rSSI_SR_M_2						 (0x28 )
-#define rSSI_IMR_M_2						 (0x2C )
-#define rSSI_ISR_M_2						 (0x30 )
-#define rSSI_RISR_M_2						 (0x34 )
-#define rSSI_TXOICR_M_2						 (0x38 )
-#define rSSI_RXOICR_M_2						 (0x3C )
-#define rSSI_RXUICR_M_2						 (0x40 )
-#define rSSI_MSTICR_M_2						 (0x44 )
-#define rSSI_ICR_M_2						 (0x48 )
-#define rSSI_DMACR_M_2						 (0x4C )
-#define rSSI_DMATDLR_M_2					 (0x50 )
-#define rSSI_DMARDLR_M_2					 (0x54 )
-#define rSSI_IDR_M_2						 (0x58 )
-#define rSSI_VERSION_M_2					 (0x5C )
-#define rSSI_DR_M_2						 (0x60 )
-
-
-
-//========================================================================
-// SSI SLAVE 
-//========================================================================
-#define rSSI_CTLR0_S						 (0x0 )
-#define rSSI_CTLR1_S						 (0x4 )
-#define rSSI_ENR_S						 (0x8 )
-#define rSSI_MWCR_S						 (0xC )
-#define rSSI_SER_S						 (0x10 )
-#define rSSI_BAUDR_S						 (0x14 )
-#define rSSI_TXFTLR_S						 (0x18 )
-#define rSSI_RXFTLR_S						 (0x1C )
-#define rSSI_TXFLR_S						 (0x20 )
-#define rSSI_RXFLR_S						 (0x24 )
-#define rSSI_SR_S						 (0x28 )
-#define rSSI_IMR_S						 (0x2C )
-#define rSSI_ISR_S						 (0x30 )
-#define rSSI_RISR_S						 (0x34 )
-#define rSSI_TXOICR_S						 (0x38 )
-#define rSSI_RXOICR_S						 (0x3C )
-#define rSSI_RXUICR_S						 (0x40 )
-#define rSSI_MSTICR_S						 (0x44 )
-#define rSSI_ICR_S						 (0x48 )
-#define rSSI_DMACR_S						 (0x4C )
-#define rSSI_DMATDLR_S					 	 (0x50 )
-#define rSSI_DMARDLR_S					 	 (0x54 )
-#define rSSI_IDR_S						 (0x58 )
-#define rSSI_VERSION_S					 	 (0x5C )
-#define rSSI_DR_S						 (0x60 )
+#define IMAP_MSSI0 ((IMAP_SSI_TypeDef*)MSSI0_BASE)
+#define IMAP_MSSI1 ((IMAP_SSI_TypeDef*)MSSI1_BASE)
+#define IMAP_MSSI2 ((IMAP_SSI_TypeDef*)MSSI2_BASE)
+#define IMAP_SSSI  ((IMAP_SSI_TypeDef*)SSSI_BASE)
 
 #endif
