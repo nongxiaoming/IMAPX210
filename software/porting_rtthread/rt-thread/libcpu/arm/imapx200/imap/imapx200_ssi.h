3eb7b4d5796e08ba9d61e6662f00576ff62757a8
xiaonong <nongxiaoming@gmail.com>
2014-11-30 00:47:06 +0800

    修正ssi寄存器定义bug。

--- software/porting_rtthread/rt-thread/libcpu/arm/imapx200/imap/imapx200_ssi.h
+++ software/porting_rtthread/rt-thread/libcpu/arm/imapx200/imap/imapx200_ssi.h
@@ -8,29 +8,31 @@
 
 typedef struct
 {
- unsigned long CTLR0;
- unsigned long CTLR1;
- unsigned long ENR;
- unsigned long MWCR;
- unsigned long SER;
- unsigned long BAUDR;
- unsigned long TXFTLR;
- unsigned long RXFTLR;
- unsigned long TXFLR;
- unsigned long RXFLR;
- unsigned long SR;
- unsigned long IMR;
- unsigned long ISR;
- unsigned long RISR;
- unsigned long TXOICR;
- unsigned long RXOICR;
- unsigned long RXUICR;
- unsigned long MSTICR;
- unsigned long ICR;
- unsigned long DMACR;
- unsigned long DMATDLR;
- unsigned long DMARDLR;
- unsigned long DR;
+	unsigned long CTRLR0;   /* RW Control Register 0 */
+	unsigned long CTRLR1;   /*  RW Control Register 1 */
+	unsigned long MSSIENR;  /*  RW MSSI Enable Register */
+	unsigned long MWCR;     /*  RW Microwire Control Register */
+	unsigned long SER;      /*  RW Slave Enable Register Width : MSSI_NUM_SLAVES */
+	unsigned long BAUDR;    /*  RW Baud Rate Select */
+	unsigned long TXFTLR;   /*  RW Transmit FIFO Threshold Level */
+	unsigned long RXFTLR;   /*  RW Receive FIFO Threshold Level */
+	unsigned long TXFLR;    /*  RW Transmit FIFO Level Register Width : TX_ABW + 1 */
+	unsigned long RXFLR;    /*  RW Receive FIFO Level Register Width : RX_ABW + 1 */
+	unsigned long SR;       /*  R Status Register */
+	unsigned long IMR;      /*  RW Interrupt Mask Register */
+	unsigned long ISR;      /*  R Interrupt Status Register */
+	unsigned long RISR;     /*  R Raw Interrupt Status Register */
+	unsigned long TXOICR;   /*  R Transmit FIFO Overflow Interrupt Clear Register */
+	unsigned long RXOICR;   /*  R Receive FIFO Overflow Interrupt Clear Register */
+	unsigned long RXUICR;   /*  R Receive FIFO Underflow Interrupt Clear Register */
+	unsigned long MSTICR;   /*  R Multi - Master Interrupt Clear Register */
+	unsigned long ICR;      /*  R Interrupt Clear Register */
+	unsigned long DMACR;    /*  RW DMA Control Register */
+	unsigned long DMATDLR;  /*  RW DMA Transmit Data Level */
+	unsigned long DMARDLR;  /*  RW DMA Receive Data Level */
+	unsigned long IDR;
+	unsigned long VERSION;
+	unsigned long DR;       /*  RW Data Register */
 }IMAP_SSI_TypeDef;
 
 #define IMAP_MSSI0 ((IMAP_SSI_TypeDef*)MSSI0_BASE)
